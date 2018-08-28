6ad17f9140cde96ea972a6e3cc8b54c1d8b9d9fa
David Suarez <dsuarey@gmail.com>
2018-08-28 11:17:51 -0700

    Display circles, colors, functionality

--- main.js
+++ main.js
@@ -1,11 +1,59 @@
 console.log("JS loaded.");
 
+/*
+ *  CONSTANTS, STATE and CACHED DOM ELEMENTS
+ */
+
 document.turn = "Player 1";
+const container = document.getElementById('container');
+const state = new Array(42).fill(0);
+const boxes = document.querySelectorAll('.box');
 
-let container = document.getElementById('container');
+/*
+ *  EVENT LISTENERS
+ */
 
-container.addEventListener('click', function(e){
-    e.target.innerText = "HI";
-    // console.log(e.target.)
-});
+container.addEventListener('click', dropCircle);
 
+/*
+ *  UTILITY FUNCTIONS
+ */
+
+function dropCircle(evt) {
+    let column = evt.target.classList[1].split("-")[1];
+    let colBottomIdx = 35 + column;
+    for (var start=colBottomIdx; start>=0; start-=7) {
+        if (state[start] === 0) {
+            state[start] = document.turn === "Player 1" ? 1 : -1;
+            document.turn = document.turn === "Player 1" ? "Player 2" : "Player 1";
+            render();
+            return;
+        }   
+    } 
+}
+
+/*
+ *  UTILITY FUNCTIONS
+ */
+
+function winner() {
+    // Write code here
+    // winner should look at state and determine if there's a winner
+    // if there are 4 in a row for P1 return 1; P2 return -1; else return 0
+    if () {
+        
+    } else if {
+
+    } else {
+
+    }
+
+}
+
+function render() {
+    for (let i=0; i<state.length; i++) {
+        if (state[i] !== 0) {
+            boxes[i].classList.add(state[i] === 1 ? "red": "green"); 
+        }
+    }
+}
